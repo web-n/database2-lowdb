49b4b9e7c303935f4a91f26998e1447858fb5999
egoing <egoing@gmail.com>
2018-08-11 16:40:05 +0900

    9.비동기처리

--- main.js
+++ main.js
@@ -1,64 +1,72 @@
 var shortid = require('shortid');
 var low = require('lowdb')
-var FileSync = require('lowdb/adapters/FileSync')
-var adapter = new FileSync('db.json')
-var db = low(adapter)
-// Set some defaults (required if your JSON file is empty)
-db.defaults({
-    topic: [],
-    author: []
-}).write();
+var FileAsync = require('lowdb/adapters/FileAsync')
+var adapter = new FileAsync('db.json')
+low(adapter).then(
+    function (db) {
+        // Set some defaults (required if your JSON file is empty)
+        db.defaults({
+            topic: [],
+            author: []
+        }).write();
 
-// db.get('author').push({
-//     id:1,
-//     name:'egoing',
-//     profile:'developer' 
-// }).write();
 
-// db.get('topic').push({
-//     id:1,
-//     title:'lowdb',
-//     description:'lowdb is ...',
-//     author:1
-// }).write();
+        // db.get('author').push({
+        //     id:1,
+        //     name:'egoing',
+        //     profile:'developer' 
+        // }).write();
 
-// db.get('topic').push({
-//     id: 2,
-//     title: 'mysql',
-//     description: 'mysql is ...',
-//     author: 1
-// }).write();
+        // db.get('topic').push({
+        //     id:1,
+        //     title:'lowdb',
+        //     description:'lowdb is ...',
+        //     author:1
+        // }).write();
 
-// console.log(
-//     db.get('topic')
-//     .find({title:'lowdb', author:1})
-//     .value()
-// );
+        // db.get('topic').push({
+        //     id: 2,
+        //     title: 'mysql',
+        //     description: 'mysql is ...',
+        //     author: 1
+        // }).write();
 
-// db.get('topic')
-//     .find({id:2})
-//     .assign({title:'MySQL & MariaDB'})
-//     .write();
+        // console.log(
+        //     db.get('topic')
+        //     .find({title:'lowdb', author:1})
+        //     .value()
+        // );
 
-// db.get('topic')
-//     .remove({
-//         id: 2
-//     })
-//     .write();
+        // db.get('topic')
+        //     .find({id:2})
+        //     .assign({title:'MySQL & MariaDB'})
+        //     .write();
 
-var sid = shortid.generate();
-db.get('author')
-    .push({
-        id:sid,
-        name:'taeho',
-        profile:'data scientist'
-    }).write();
+        // db.get('topic')
+        //     .remove({
+        //         id: 2
+        //     })
+        //     .write();
 
-db.get('topic')
-    .push({
-        id:shortid.generate(),
-        title:'PostgreSQL',
-        description:'PostgreSQL is ...',
-        author:sid
-    }).write();
-    
+        var sid = shortid.generate();
+        db.get('author')
+            .push({
+                id: sid,
+                name: 'duru',
+                profile: 'db admin'
+            }).write().then(function(){
+                console.log('add author');
+            });
+
+        db.get('topic')
+            .push({
+                id: shortid.generate(),
+                title: 'MSSQL',
+                description: 'MSSQL is ...',
+                author: sid
+            }).write().then(function(){
+                console.log('add topic');
+            });
+
+    }
+)
